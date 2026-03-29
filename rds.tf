3b8a30227726ea606c7d0e7df21bd48643c3a2a4
pravinmenghani1 <34215341+pravinmenghani1@users.noreply.github.com>
2026-03-29 19:12:08 +0530

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-west-2"
+}
+resource "aws_db_instance" "example" {
+ allocated_storage = 20
+ engine = "mysql"
+ engine_version = "8.0"
+ instance_class = "db.t3.micro"
+ db_name = "mydb"
+ username = "admin"
+ password = "password"
+ parameter_group_name = "default.mysql8.0"
+ skip_final_snapshot = true
+}
